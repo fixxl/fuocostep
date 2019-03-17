6d837aad5656f6ad0bd3d155c8d52e807bee5481
Felix Pflaum <f.pflaum@gmail.com>
2019-03-17 14:52:52 +0100

    Trifle

--- Bootloader/flsh.bat
+++ Bootloader/flsh.bat
@@ -1,2 +1,1 @@
-avrdude.exe -pm328p -cavrispmkII -Pusb -u -Uflash:w:Bootloader_Fuocostep.hex:a -Ulfuse:w:0xe0:m -Uhfuse:w:0xd6:m -Uefuse:w:0xfd:m
-pause
+avrdude.exe -pm328p -cavrispmkII -Pusb -u -Uflash:w:Bootloader_Fuocostep.hex:a -Ulfuse:w:0xe0:m -Uhfuse:w:0xd6:m -Uefuse:w:0xfd:m && sleep 5
